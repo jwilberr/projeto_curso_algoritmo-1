6c9c739b41968b6497f9de92cfd023ced1ea847a
wilber ribeiro <jwilber1972@gmail.com>
2020-04-28 16:06:18 -0300

    Removido os comentarios do fonte

--- concatenacao.js
+++ concatenacao.js
@@ -1,20 +1,3 @@
-/* Var
-// Seção de Declarações das variáveis 
-   nome: caractere
-   numero: real
-   
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-     escreval("Digite um nome: ")
-     leia(nome)
-     escreval("Digite um número: ")
-     leia(numero)
-     
-     escreval(nome, ":" , numero)
-
-Fimalgoritmo */
-
 var nome, numero
 nome = prompt("Digite seu nome: ")
 numero = prompt("Digite seu numero: ")
--- estrutura-enquanto.js
+++ estrutura-enquanto.js
@@ -1,32 +1,3 @@
-/*
-Var
-// Seção de Declarações das variáveis
-   nome: caractere
-   idade, limite, contador: inteiro
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   escreva("Digite a quantidade de vezes que vai ser verificado a idade: ")
-   leia(limite)
-   contador :=0
-// inicio do enquanto:
-   enquanto contador < limite faca
-            escreva ("Digite o nome da pessoa: ")
-            leia (nome)
-            escreva ("Digite a idade de " , nome, ":")
-            leia(idade)
-//  início do se:
-           se idade > 18 entao
-              escreval (nome, " - você é maior de idade!")
-           senao
-              escreval (nome, " - você é menor de idade!")
-// fim do se:
-           fimse
-           contador := contador + 1
-// fim do enquanto:
-   fimenquanto
-
-Fimalgoritmo */
-
 function acaoBotao() {
     var nome, idade, limite, contador
     limite = prompt("Digite a quantidade de vezes que vai ser verificado a idade: ")
